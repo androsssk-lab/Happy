bb06542f8b46ceaaf890a1353f57715a0b653428
androsssk-lab <androsssk@gmail.com>
2026-02-12 21:49:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const burger = document.querySelector('.burger');
 const burger_menu = document.querySelector('.burger_menu');
+const burger_items = document.querySelectorAll('.burger_list-items');
 
 burger.addEventListener('click', () => {
     burger_menu.classList.toggle('active');
+});
+
+burger_items.forEach(item => {
+    item.addEventListener('click', () => {
+        burger_menu.classList.remove('active');
+    });
 });
